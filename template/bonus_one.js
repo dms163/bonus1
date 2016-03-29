bc19d290727d7b80cb62735d6e151372ec9a6425
dms163 <dms163@pitt.edu>
2016-03-29 00:10:29 -0400

    add css and js files

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -12,6 +12,46 @@
 // keyup events could be helpful to get value of field as the user types
 
 (function() {
-	// Magic!
-	console.log('Keepin\'n it clean with an external script!');
+	// Magic
+	$.ajaxSetup({
+		async: false
+	});
+
+	var json = $.getJSON("http://www.mattbowytz.com/simple_api.json",{data: "all"}, function( json ) {
+		console.log(json);
+		return json;
+	});
+	var parsed = JSON.parse(json.responseText);
+	var programming = parsed.data.programming;
+	var interests = parsed.data.interests;
+
+
+
+	$(".flexsearch").keyup(function() {
+
+		$(".matches").remove();
+		var prefixes = [];
+		var input = $('.flexsearch-input').val();
+		var inputLength = input.length;
+		for(var i=0; i<interests.length; i++){
+			var substring = interests[i].substring(0, inputLength).toLowerCase();
+			if(substring == input) {
+				prefixes.push(interests[i]);
+			}
+		}
+		for(var i=0; i<programming.length; i++){
+			var substring = programming[i].substring(0, inputLength).toLowerCase();
+			if(substring === input){
+				prefixes.push(programming[i]);
+			}
+		}
+		$(".flexsearch").append("<ul class='matches'></ul>");
+		if(prefixes.length>8){
+			$(".matches").remove();
+		}
+		for(var i=0; i<prefixes.length; i++){
+			var link = "http://www.google.com/search?q=" + prefixes[i].replace("","+") + ">" + prefixes[i];
+			$(".matches").append("<li><a href=" + link + "</a></li>");
+		}
+	});
 })();
